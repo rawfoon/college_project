112256f116f40f9d9c1a78d53c28db3459496788
rawfoon <mgrawfoon@gmail.com>
2024-02-12 01:32:48 +0600

    card image rounded 50%

--- src/app/components/Main/Cards.jsx
+++ src/app/components/Main/Cards.jsx
@@ -6,7 +6,7 @@ const Cards = () => {
 
             <div className="grid grid-cols-1 gap-8 md:grid-cols-3 container mx-auto ">
             <div className=" p-6 rounded-xl shadow-xl bg-white dark:bg-gray-900 dark:text-gray-50">
-	<img src="https://www.gdc.gov.bd/midea/featuredimage/featuredimage2023-03-25-17-10-31_641f2b07d57e9.jpg" alt="" className="rounded-[50%] object-center mx-auto rounded-md h-72 dark:bg-gray-500" />
+	<img src="https://www.gdc.gov.bd/midea/featuredimage/featuredimage2023-03-25-17-10-31_641f2b07d57e9.jpg" alt="" className=" object-center mx-auto rounded-[50%] h-72 dark:bg-gray-500" />
 	<div className="mt-6 mb-2">
 		
 		<h2 className="text-xl font-semibold tracki">DR. MD. Mehadi Hasan</h2>
@@ -15,7 +15,7 @@ const Cards = () => {
 	<p className="dark:text-gray-100">Gurudayal Govt. College</p>
 </div>
             <div className=" p-6 rounded-xl shadow-xl bg-white dark:bg-gray-900 dark:text-gray-50">
-	<img src="https://www.gdc.gov.bd/midea/featuredimage/featuredimage2023-04-02-02-28-20_6428e844c32de.jpg" alt="" className=" rounded-[50%] object-center mx-auto rounded-md h-72 dark:bg-gray-500" />
+	<img src="https://www.gdc.gov.bd/midea/featuredimage/featuredimage2023-04-02-02-28-20_6428e844c32de.jpg" alt="" className="  object-center mx-auto rounded-[50%] h-72 dark:bg-gray-500" />
 	<div className="mt-6 mb-2">
 	
 		<h2 className="text-xl font-semibold tracki">A. N. M. Mustakur Rahman</h2>
@@ -24,7 +24,7 @@ const Cards = () => {
 	<p className="dark:text-gray-100">Gurudayal Govt. College</p>
 </div>
             <div className=" p-6 rounded-xl shadow-xl bg-white dark:bg-gray-900 dark:text-gray-50">
-	<img src="https://i.ibb.co/R3cfBFr/omar-faruk.jpg" alt="" className="rounded-[50%] object-center mx-auto rounded-md h-72 dark:bg-gray-500" />
+	<img src="https://i.ibb.co/R3cfBFr/omar-faruk.jpg" alt="" className=" object-center mx-auto rounded-[50%] h-72 dark:bg-gray-500" />
 	<div className="mt-6 mb-2">
 		
 		<h2 className="text-xl font-semibold tracki">Prof. Md. Omar Faroque</h2>
